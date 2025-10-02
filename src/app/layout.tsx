26f3caafc5179c02a446851b80e4f15fe924183a
Meetj28 <meetj.jain576@gmail.com>
2025-10-02 17:07:42 +0530

    code clean

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { FavoritesProvider } from "@/context/FavoritesContext";
-import { ThemeProvider } from "@/context/ThemeContext";
 import Header from "@/components/layout/Header";
 import Container from "@/components/layout/Container";
 import "./globals.css";
@@ -10,12 +9,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en" suppressHydrationWarning>
       <body className="bg-gray-50 text-gray-900 dark:bg-gray-950 dark:text-gray-100">
-        <ThemeProvider>
           <FavoritesProvider>
             <Header />
             <Container>{children}</Container>
           </FavoritesProvider>
-        </ThemeProvider>
       </body>
     </html>
   );
