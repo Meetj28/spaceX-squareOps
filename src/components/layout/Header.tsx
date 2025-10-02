82b4e5c4db6ddf986099ee1b69c8397863e762cb
Meetj28 <meetj.jain576@gmail.com>
2025-10-02 17:02:01 +0530

    Remove unwanted codes

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,18 +1,12 @@
 "use client";
 
 import Link from "next/link";
-import { useTheme } from "@/context/ThemeContext";
-import Button from "../ui/Button";
 
 const staticText = {
   title: "🚀 SpaceX Mission Explorer",
-  toggleDark: "🌙 Dark",
-  toggleLight: "☀️ Light",
 };
 
 export default function Header() {
-  const { theme, toggleTheme } = useTheme();
-
   return (
     <header className="w-full border-b border-gray-200 bg-white dark:bg-gray-900 dark:border-gray-800">
       <div className="mx-auto flex max-w-7xl items-center px-4 py-3 sm:px-6 lg:px-8">
@@ -25,15 +19,6 @@ export default function Header() {
             {staticText.title}
           </Link>
         </div>
-
-        {/* Light and Dark Mode Button, functionality remaining}
-        
-        {/* Right side */}
-        {/* <div className="flex w-1/2 items-center justify-end gap-6">
-          <Button variant="secondary" onClick={toggleTheme}>
-            {theme === "light" ? staticText.toggleDark : staticText.toggleLight}
-          </Button>
-        </div> */}
       </div>
     </header>
   );
